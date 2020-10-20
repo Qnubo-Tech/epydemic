a5ae84cd5e7f5514b7c0b6d5f9c3f65efc567b2c
NonoMalpi <antoniomalpicamorales@gmail.com>
2020-10-20 19:41:59 +0200

    feat: Add methods to plot lines or stacked areas for society progress

--- src/simulation/graph.py
+++ src/simulation/graph.py
@@ -1,6 +1,8 @@
-from typing import Dict
+from typing import Dict, List
 
 import matplotlib.pyplot as plt
+
+from src.environment.status import Status
 
 
 class Graph:
@@ -31,11 +33,59 @@ class Graph:
 
         ax.axis('off')
         table = ax.table(cellText=cells,
-                           cellLoc='center',
-                           rowLabels=list(params_dict.keys()),
-                           rowLoc='center',
-                           colWidths=[0.1],
-                           loc='center')
+                         cellLoc='center',
+                         rowLabels=list(params_dict.keys()),
+                         rowLoc='center',
+                         colWidths=[0.1],
+                         loc='center')
 
         table.auto_set_font_size(False)
         table.set_fontsize(10)
+
+    @staticmethod
+    def plot_lines_society_progress(ax: plt.axis,
+                           time_array: List,
+                           society_snapshot: Dict,
+                           society_progress: Dict):
+
+        for st in Status:
+            society_progress[st.name].append(society_snapshot[st.name] / society_snapshot["Total"])
+            ax.plot(
+                time_array, society_progress[st.name],
+                c=st.value, ls='-', label=st.name, alpha=0.5
+            )
+            ax.text(
+                x=time_array[-1], y=society_progress[st.name][-1],
+                s=r"{0:.2f}".format(society_progress[st.name][-1]),
+                size=10,
+                color=st.value
+            )
+
+    @staticmethod
+    def plot_areas_society_progress(ax: plt.axis,
+                                   time_array: List,
+                                   society_snapshot: Dict,
+                                   society_progress: Dict):
+
+        previous_status = None
+        for st in Status:
+            society_progress[st.name].append(society_snapshot[st.name])
+
+            if previous_status:
+                lower_limit = society_progress[previous_status.name]
+            else :
+                lower_limit = [0]
+
+            ax.fill_between(
+                x=time_array, y1=lower_limit, y2=society_progress[st.name],
+                color=st.value, label=st.name, alpha=0.25
+            )
+
+            ax.text(
+                x=time_array[-1], y=1 / 2 * (lower_limit[-1] + society_snapshot[st.name]),
+                s=r"{0:.2f}".format(society_snapshot[st.name] - lower_limit[-1]),
+                size=10,
+                color=st.value
+            )
+
+            previous_status = st
--- src/simulation/run.py
+++ src/simulation/run.py
@@ -31,12 +31,6 @@ def run():
 
     fig, axis = Graph.generate_fig_ax(show_params=PLOT_PARAMETERS)
 
-    # fig = plt.figure()
-    # gs = fig.add_gridspec(3, 4)
-    # ax1 = fig.add_subplot(gs[:, -2:])
-    # ax2 = fig.add_subplot(gs[1:, :-2])
-    # ax3 = fig.add_subplot(gs[0, :-2])
-
     times = []
     society_progress = {st.name: [] for st in Status}
     time = 0
@@ -71,33 +65,14 @@ def run():
             cumulative_status = society.get_cumulative_status()
 
             times.append(time / 3600 / 24)
-            previous_status = None
-            for st in Status:
-                # lines
-                #society_progress[st.name].append(society_status[st.name] / society_status["Total"])
-                #axis[0].plot(times, society_progress[st.name], c=st.value, ls='-', label=st.name)
-
-                # stacked areas
-                society_progress[st.name].append(cumulative_status[st.name])
-
-                if previous_status:
-                    lower_limit = society_progress[previous_status.name]
-                else:
-                    lower_limit = [0]
-
-                axis[0].fill_between(
-                    x=times, y1=lower_limit, y2=society_progress[st.name],
-                    color=st.value, label=st.name, alpha=0.25
-                )
-
-                axis[0].text(
-                    x=times[-1], y=1/2 * (lower_limit[-1] + cumulative_status[st.name]),
-                    s=r"{0:.2f}".format(cumulative_status[st.name] - lower_limit[-1]),
-                    size=10,
-                    color=st.value
-                )
-
-                previous_status = st
+            Graph.plot_areas_society_progress(ax=axis[0],
+                                              time_array=times,
+                                              society_snapshot=cumulative_status,
+                                              society_progress=society_progress)
+            # Graph.plot_lines_society_progress(ax=axis[0],
+            #                          time_array=times,
+            #                          society_snapshot=society_status,
+            #                          society_progress=society_progress)
 
             axis[0].set_ylim(0, Geometry.Box.Ly)
             axis[0].legend(loc=2)
